b0bf2b3df1617205fa81e4dac404a311b7dbd84f
RediChen <a0929052958@gmail.com>
2024-05-29 17:13:42 +0800

    建立 dashboard 基本架構

---
+++ dashboard/dashboard_temp.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="zh-Hant-tw">
+<head>
+    <title>資料表展示</title>
+    <?php include("../../common-head.php"); ?>
+
+<style>
+    body {
+        background-color: #333;
+        color: #eee;
+    }
+</style>
+</head>
+
+<body>
+    <div class="container">
+        <div>
+            <a href="./course.php" class="btn btn-primary">課程管理</a>
+        </div>
+    </div>
+</body>
+
+</html>
