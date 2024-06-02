6074394873cf52fa9b6a7d0ed1e3954930bb0e6b
RediChen <a0929052958@gmail.com>
2024-06-03 00:29:09 +0800

    建置通用 lib 檔

---
+++ tools/console-lib.php
@@ -0,0 +1,40 @@
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap');
+
+    pre {
+        padding: 1rem;
+        background-color: #002c0d;
+        color: #fdfcfa;
+        border-radius: 1rem;
+        font-family: "IBM Plex Mono", "Helvetica Neue";
+        text-align: left;
+    }
+</style>
+<?php
+function printArray($array)
+{
+    echo '<p><pre>';
+    print_r($array);
+    echo '</pre></p>';
+}
+
+function printSess()
+{
+    if (isset($_SESSION['user'])) :
+        printArray($_SESSION);
+    else :
+        echo '<br>沒有 session<br>';
+    endif;
+}
+
+function br($times = 1)
+{
+    for ($i = 0; $i < $times; $i++) {
+        echo '<br>';
+    }
+}
+
+function leadTo($path)
+{
+    header("Location: $path");
+}
---
+++ tools/printSession.php
@@ -0,0 +1,39 @@
+<?php
+include('./console-lib.php');
+session_start();
+?>
+
+<head>
+    <title>SESSION</title>
+    <?php include("./common-head.php"); ?>
+    <link rel="shortcut icon" href="./favicon_S.png" type="image/x-icon">
+    <style>
+        /* 思源黑體 */
+        @import url("https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100..900&display=swap");
+
+        body {
+            background-color: #333;
+            color: #eee;
+            font-family: "Noto Sans TC", "Helvetica Neue";
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="text-center mt-5">以下為 $_SESSION 的所有內容：</h1>
+        <?php
+        printArray($_SESSION);
+        ?>
+    </div>
+    <div class="text-center mt-3">
+        <h3>點擊以清除 SESSION</h3>
+        <a href="?clear=true" class="btn btn-danger">清除 SESSION</a>
+    </div>
+</body>
+<?php
+if (isset($_GET['clear']) && $_GET['clear']) :
+    session_destroy();
+    leadTo("./printSession.php");
+endif;
+?>
