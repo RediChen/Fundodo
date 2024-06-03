e76198b836bf82a92bf52041a5c704c38c84316a
RediChen <a0929052958@gmail.com>
2024-06-03 17:26:08 +0800

    建立介面模板

---
+++ course/interface_T.php
@@ -0,0 +1,23 @@
+<?php
+$to_fdd = "/xampp/htdocs/Fundodo/";
+require_once("/xampp/htdocs/connectDB_fdd.php");
+include($to_fdd . "tools/tool-lib.php");
+//todo 本地測試用
+include($to_fdd . "tools/console-lib.php");
+?>
+<!DOCTYPE html>
+<html lang="zh-Hant-tw">
+
+<head>
+    <title>XXXX | Fundodo</title>
+    <?php include($to_fdd . "tools/common-head.php"); ?>
+</head>
+
+<body>
+    <div class="container">
+
+    </div>
+
+</body>
+
+</html>
