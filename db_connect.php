2db3d41b72ec3b57b55506210a646a9479f31d15
notnerdyy <liiaaxxli@gmail.com>
2024-06-05 15:31:03 +0800

    修改更新

--- db_connect.php
+++ db_connect.php
@@ -2,17 +2,21 @@
 // 共用
 $server_name="localhost";
 $user_name="admin";
-$database = "fundodo";
+$db_name = "fundodo";
 
 //* 泓宣
 $password="12345";
 $db_name="db_test";
 
+//* 李柔
+$password = "12345";
+$db_name = "pet_hotel_db";
+
 //*陳致
-$password = ".glWcQbgMsmuL2rB";
+// $password = ".glWcQbgMsmuL2rB";
 
-$connect=new mysqli($server_name,$user_name,$password,$db_name);
+$conn=new mysqli($server_name,$user_name,$password,$db_name);
 
-if ($connect->connect_error) {
-    die("連線失敗: " . $connect->connect_error);
+if ($conn->connect_error) {
+    die("連線失敗: " . $conn->connect_error);
 }
