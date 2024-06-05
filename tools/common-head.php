ab819ea0c1fcb4f6e26a2f03db5426b7b91722d4
RediChen <a0929052958@gmail.com>
2024-06-05 12:15:51 +0800

    將字型匯入併入 common-head

--- tools/common-head.php
+++ tools/common-head.php
@@ -8,5 +8,10 @@
 <!-- cdn : animate CSS -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
 
-<!-- 專案的 CSS(總集) -->
+<!-- 字體：中文的 NOTO 與英文的 Roboto -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+
+<!-- 本專案的 CSS(總集) -->
 <link rel="stylesheet" href="/fundodo/style.css">
