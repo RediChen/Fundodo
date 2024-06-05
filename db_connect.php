89979fd01dc8c2f058dfadcec4f26c1f0679748a
RediChen <a0929052958@gmail.com>
2024-06-05 12:19:08 +0800

    db-connect 更新為專案共用模式

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,15 @@
 <?php
+// 共用
 $server_name="localhost";
 $user_name="admin";
+$database = "fundodo";
+
+//* 泓宣
 $password="12345";
 $db_name="db_test";
+
+//*陳致
+$password = ".glWcQbgMsmuL2rB";
 
 $connect=new mysqli($server_name,$user_name,$password,$db_name);
 
