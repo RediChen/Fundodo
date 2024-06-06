75e8b3d9ab04b2503d8b2ba35cda54be4480a595
shiba10222 <thilder10222@gmail.com>
2024-06-06 14:48:47 +0800

    加上products資料庫

--- db_connect.php
+++ db_connect.php
@@ -7,7 +7,7 @@ $db_name = "fundodo";
 
 //* 泓宣 宏榮
 $password="12345";
-$db_name="db_test";
+// $db_name="db_test";
 
 //* 李柔
 $password = "12345";
@@ -25,7 +25,7 @@ $password = "12345";
 // $dbname = "pet_hotel_db";
 
 //* 晨宇
-// $db_name="db-products";
+$db_name="db-products";
 $conn=new mysqli($server_name,$user_name,$password,$db_name);
 
 if ($conn->connect_error) {
