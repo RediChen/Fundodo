71032b1b7a0180e521265e60985b53821454244c
RediChen <a0929052958@gmail.com>
2024-06-10 00:29:53 +0800

    將圖片命名函數納入 tool-lib

--- tools/tool-lib.php
+++ tools/tool-lib.php
@@ -1,7 +1,18 @@
 <?php
-function leadTo($path)
+function leadTo(string $path)
 {
     header("Location: $path");
     exit;
+}
+/**
+ * @description 生成歸檔圖片的格式化檔名
+ * @param string $genre:兩個大寫字母以代表網站區域
+ * @param int $id: 對應的項目 id
+ * @param int $i_img: 圖片在該項目所屬下的子序號
+ * @param string $format: 圖片的副檔名
+ */
+function formatFileName(string $genre,int $id, int $i_img, string $format)
+{
+    return $genre . sprintf("%06d", $id) . $i_img . '.' . $format;
 }
 ?>
