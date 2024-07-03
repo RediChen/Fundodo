1327ff7e128e6f7e25a80bed1d53da87596a65f5
RediChen <a0929052958@gmail.com>
2024-07-04 00:15:30 +0800

    toy 格式整理階段

--- lab/prod_toys/injToy.php
+++ lab/prod_toys/injToy.php
@@ -1,13 +1,28 @@
 <?php
 // require_once('/tools/connectDB_fdd.php');
-include("/tools/console-lib.php");
-$json = file_get_contents('./toy-raw.json');
-$pkg = json_decode($json, true);
-// {$colArr, $toyArr}
+include("../../tools/console-lib.php");
+$jsonStr = file_get_contents('./test.json');
+$pkg = json_decode($jsonStr, true);
 $colArr = $pkg['col'];
-$toyArr = $pkg['data'];
+$rawArr = $pkg['data'];
 //todo 把產品名稱中的品牌拿掉
 
-printArray($colArr);
+// printArray($colArr);
 
-printArray($toyArr[50]);
+// printArray($rawArr[50]);
+
+// $temp = $rawArr[50];
+
+// $newName = substr($temp[2], strlen($temp[1]));
+
+// printArray($newName);
+$toyArr = [];
+foreach ($rawArr as $toy) :
+  $newItem = [];
+  for ($i = 0; $i < 8; $i++) :
+    $newItem[$colArr[$i]] = $toy[$i];
+  endfor;
+  array_push($toyArr, $newItem);
+endforeach;
+
+printArray($toyArr);
